1d866634d4ed97564391fb878c62b7d737da8d5d
xuxiaoping <xuxp2018@163.com>
2019-01-07 20:16:28 +0800

    tran setting

---
+++ train.sh
@@ -0,0 +1,1 @@
+python train_imgreid_xent_htri.py -s market1501  -t market1501  --height 128 --width 64 --optim adam  --label-smooth  --lr 0.003  --max-epoch 60  --stepsize 20 40 --train-batch-size 128 --test-batch-size 128 -a resnet50 --eval-freq 20 --save-dir log/market1501-xent-htri
